4840caeff5ecc2c6dc87008ed246a0847aaaa4d3
Drewrey Lupton <drewrey@users.noreply.github.com>
2025-05-03 00:15:03 -0400

    Add pyright, fix some types

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main():
     commits_by_path = calculate_commits_by_path(paths, args.days)
     print_results(commits_by_path)
 
-def calculate_commits_by_path(paths: list[str], days: int) -> dict[str, int]:
+def calculate_commits_by_path(paths: list[str], days: int) -> list[tuple[str, int]]:
     res = {}
     for path in paths:
         commits = get_commits_for_repo(path, days)
@@ -34,12 +34,12 @@ def calculate_commits_by_path(paths: list[str], days: int) -> dict[str, int]:
         res[path] = total_commits
     return sorted(res.items(), key=lambda item: item[1], reverse=True)
 
-def print_results(commits_by_path: dict[str, int]):
+def print_results(commits_by_path: list[tuple[str, int]]):
     print("----------------")
     print("Commits per repo")
     print("----------------")
     for k, v in commits_by_path:
-        if v > 0:
+        if int(v) > 0:
             print(f"{k.split("/")[-1]}: {v}")
 
 def find_directories(parent_dir="./") -> list[str]:
@@ -59,7 +59,7 @@ def is_git_repo(path) -> bool:
         logging.debug(f"Directory not git repo: {path}")
         return False
 
-def get_commits_for_repo(repo_path="./", days=30) -> dict[date, int]:
+def get_commits_for_repo(repo_path="./", days=30) -> list[tuple[date, int]]:
     repo = Repo(repo_path)
 
     end_date = datetime.datetime.now()
@@ -68,7 +68,7 @@ def get_commits_for_repo(repo_path="./", days=30) -> dict[date, int]:
     commit_counts = defaultdict(int)
 
     if not has_commits(repo):
-        return {}
+        return []
 
     for commit in repo.iter_commits(since=start_date, until=end_date):
         commit_date = datetime.datetime.fromtimestamp(commit.committed_date).date()
@@ -84,7 +84,7 @@ def has_commits(repo: Repo) -> bool:
     except ValueError:
         return False
 
-def get_total_commits(commit_counts: dict[date, int]) -> int:
+def get_total_commits(commit_counts: list[tuple[date, int]]) -> int:
     return sum([x for (k, x) in commit_counts])
 
 if __name__ == "__main__":
